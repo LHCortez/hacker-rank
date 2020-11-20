fdbdd48eac5663399d7b8a4e34c99198389c0125
LHCortez <luizcquintas@gmail.com>
2020-11-20 15:20:32 -0300

    Day 3 - Intro to Conditional Statements

---
+++ src/HackerRank/DayThree.java
@@ -0,0 +1,22 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class DayThree {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+
+        if (n % 2 != 0 || (n >= 6 && n <= 20)) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
